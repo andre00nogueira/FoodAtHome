4b988bf47f6a1f0a7f8825212769c42af41ebbff
André Nogueira Mateus da Silva <2181057@my.ipleiria.pt>
2020-12-29 15:30:54 +0000

    Added new navbar and fixed profile.

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -1,21 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
     <router-link class="navbar-brand" to="/">Food@Home</router-link>
     <router-link
-      v-if="$store.state.user && $store.state.user.type == 'C'"
+      v-if="user && user.type == 'C'"
       class="navbar-brand"
-      :to="`/customer/${$store.state.user.id}/dashboard`"
+      :to="`/customer/${user.id}/dashboard`"
       >Dashboard</router-link
     >
     <router-link
-      v-if="$store.state.user && $store.state.user.type == 'EC'"
+      v-if="user && user.type == 'EC'"
       class="navbar-brand"
-      :to="`/cook/${$store.state.user.id}/dashboard`"
+      :to="`/cook/${user.id}/dashboard`"
       >Dashboard</router-link
     >
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-        <template v-if="!$store.state.user">
+        <template v-if="!user">
           <li class="nav-item">
             <router-link to="/login" class="btn btn-primary">Login</router-link>
             <router-link to="/customers/create" class="btn btn-primary"
@@ -24,77 +24,54 @@
           </li>
         </template>
         <template v-else>
-          <!-- <li class="nav-item">
-            <img id="userPhoto" class="img-profile rounded-circle" style="width: 100px; height: 100px;" :src="`storage/fotos/${user.photo_url}`" />
-             <router-link :to="`/users/${user.id}`" class="btn btn-primary"
-              >{{user.name}}</router-link
-            >
-            <router-link
-              v-if="$store.state.user.type == 'C'"
-              to="/cart"
-              class="btn btn-primary"
-              >Cart</router-link
-            >
-            <a href="#" @click.prevent="logout" class="btn btn-secondary"
-              >Logout</a
-            >
-          </li>
-          -->
-
-          <li class="nav-item">
-            <router-link
-              v-if="$store.state.user.type == 'C'"
-              to="/cart"
-              class="btn btn-primary"
-              >Cart</router-link
-            >
-            <!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> -->
-              <div class="container-fluid">
-                <ul class="navbar-nav">
-                  <li class="nav-item dropdown">
-                    <a
-                      class="nav-link dropdown-toggle d-flex align-items-center"
-                      href="#"
-                      id="navbarDropdownMenuLink"
-                      role="button"
-                      data-mdb-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <img
-                        :src="`storage/fotos/${user.photo_url}`"
-                        class="rounded-circle"
-                        height="22"
-                        alt=""
-                        loading="lazy"
-                      />
-                    </a>
-                    <ul
-                      class="dropdown-menu"
-                      aria-labelledby="navbarDropdownMenuLink"
-                    >
-                      <li><router-link class="dropdown-item" :to="`/users/${user.id}`">Profile</router-link></li>
-                      <li><a class="dropdown-item" href="#" @click.prevent="logout">Logout</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-          <!--  </nav> -->
-          </li>
-
-          <!-- <div class="right">
-            <ul>
-              <li>
-                <a href="#">
-                  <p>
-                    {{user.name}} <br>
-                  </p>
-                  <img :src="`storage/fotos/${user.photo_url}`" >
-                  <i class="fas fa-angle-down"></i>
+          <div class="collapse navbar-collapse" id="navbar-list-4">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown" style="display:flex">
+                <img
+                    :src="`storage/fotos/${
+                      user.photo_url || 'default_avatar.jpg'
+                    }`"
+                    class="rounded-circle"
+                    style="width: 40px; height: auto"
+                  />
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  
                 </a>
+
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <router-link
+                    :to="`/customer/${user.id}/dashboard`"
+                    class="dropdown-item"
+                    >Dashboard</router-link
+                  >
+                  <router-link
+                    :to="`/users/${user.id}`"
+                    class="dropdown-item"
+                    >Profile</router-link
+                  >
+                  <router-link
+                    v-if="user.type == 'C'"
+                    to="/cart"
+                    class="dropdown-item"
+                    >Cart</router-link
+                  >
+                  <a class="dropdown-item" @click.prevent="logout" href="#"
+                    >Logout</a
+                  >
+                </div>
               </li>
             </ul>
           </div>
-          -->
         </template>
       </ul>
     </div>
@@ -142,5 +119,14 @@ export default {
 <style>
 .navbar {
   margin-bottom: 5%;
+}
+
+.rounded_circle {
+  vertical-align: middle;
+  border-radius: 50%;
+}
+
+.dropdown-toggle{
+  text-align: right;
 }
 </style>
